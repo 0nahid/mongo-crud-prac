b7985cc9154979fc9a621685c518ed5c3fe96a0e
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-02 21:46:38 +0600

    connect mongo

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,26 @@ const port = process.env.PORT || 5500;
 // middleware
 const cors = require("cors");
 app.use(express.json());
+require("dotenv").config();
+// mongodb
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z93bc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err); 
+  }
+
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
