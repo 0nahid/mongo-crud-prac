5d3e1212eca9841a121b0a76e1e2154beb72fb96
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-02 17:02:45 +0600

    added bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -9,5 +10,5 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode> 
+  </React.StrictMode>
 );
